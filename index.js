e1b48da86c7c84082b3caa4edd121caf1cf7ab42
Daniel Tan <danieltan380@gmail.com>
2023-09-28 21:38:10 -0700

    Added coordinates and called fetchISSFlyOverTimes with an anonymous function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 /*fetchMyIP((error, ip) => {
   if (error) {
@@ -9,11 +9,25 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
   console.log('It worked! Returned IP:' , ip);
 });*/
 
-fetchCoordsByIP('142.179.77.77', (error, data) => {
+/*fetchCoordsByIP('142.179.77.77', (error, data) => {
   if (error) {
     console.log("It didn't work! ", error);
     return;
   }
 
+  console.log('It worked! Returned coordinates:' , data);
+});*/
+
+const myCoords = {
+  "latitude": 48.4284207,
+  "longitude": -123.3656444
+};
+
+fetchISSFlyOverTimes(myCoords, (error, data) => {
+  if (error) {
+    console.log("It didn't work! ", error);
+    return;
+  }
+    
   console.log('It worked! Returned coordinates:' , data);
 });
