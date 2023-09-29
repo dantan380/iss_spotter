c5b80394a8dcbd489cdfef39d73ac3c253326b61
Daniel Tan <danieltan380@gmail.com>
2023-09-28 21:36:47 -0700

    Added fetchISSFlyOverTimes function

--- iss.js
+++ iss.js
@@ -50,5 +50,32 @@ const fetchCoordsByIP = function(ip, callback) {
     callback(null, {latitude, longitude});
   });
 };
+
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+    const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`
+    request(url, (error, response, body) => {
+        if(error) {
+            callback(error, null);
+            return;
+        }
+        if (response.statusCode !== 200){
+            const message = `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`
+            callback(Error(message), null);
+        }
+        const data = JSON.parse(body).response;
+        callback(null, data)
+    })
+  };
   
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
