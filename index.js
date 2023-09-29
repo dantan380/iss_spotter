5e8e3e640c1dde5a9b032284b304b1ba82e5091d
Daniel Tan <danieltan380@gmail.com>
2023-09-28 20:24:30 -0700

    Called fetchCoordsByIP function and passed in IP address and an anonymous function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 /*fetchMyIP((error, ip) => {
   if (error) {
@@ -8,3 +8,12 @@ const { fetchMyIP } = require('./iss');
 
   console.log('It worked! Returned IP:' , ip);
 });*/
+
+fetchCoordsByIP('142.179.77.77', (error, data) => {
+  if (error) {
+    console.log("It didn't work! ", error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , data);
+});
