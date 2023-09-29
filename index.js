f62f13c1623ad40b21e71ed0997ec789d0c3d72b
Daniel Tan <danieltan380@gmail.com>
2023-09-28 22:15:19 -0700

    Called nextISStimesForMyLocation with anonymous function.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 /*fetchMyIP((error, ip) => {
   if (error) {
@@ -18,7 +18,7 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
   console.log('It worked! Returned coordinates:' , data);
 });*/
 
-const myCoords = {
+/*const myCoords = {
   "latitude": 48.4284207,
   "longitude": -123.3656444
 };
@@ -30,4 +30,20 @@ fetchISSFlyOverTimes(myCoords, (error, data) => {
   }
     
   console.log('It worked! Returned coordinates:' , data);
+});*/
+const printPassTimes = function(passTimes) {
+	for (const pass of passTimes) {
+		const datetime = new Date(0);
+		datetime.setUTCSeconds(pass.risetime);
+		const duration = pass.duration;
+		console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+	}
+}
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
